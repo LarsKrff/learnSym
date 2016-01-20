3472ca981fe505a16951cbcc977daf80b07b7415
LeffDesign <info@leff-design.nl>
2016-01-20 16:43:24 +0100

    Symfony2Test

---
+++ README.md
@@ -0,0 +1,4 @@
+learnSym
+========
+
+A Symfony project created on January 20, 2016, 10:43 am.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lars
+ * Date: 20-1-2016
+ * Time: 12:33
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{myvar}")
+     */
+
+    public function showAction($myvar) {
+
+
+        return $this->render('genus/show.html.twig',
+            ['name' => $myvar
+            ]);
+
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction() {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' => $notes,
+        ];
+
+        return new JsonResponse($data);
+    }
+
+
+}
